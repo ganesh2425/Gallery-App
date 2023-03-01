a663bc1fd5e441594a2cbedcda7e7a8b9de408f8
ganesh2425 <saahoganesh023@gmail.com>
2023-03-01 15:47:42 +0530

    ok

---
+++ src/Comp.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Gallery from './Gallery';
+
+const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
+function Comp() {
+    const [search, setSearch] = useState("");
+    const [data, setData] = useState([]);
+
+    const changeHandler = (e)=>{
+        setSearch(e.target.value)
+    }
+    const submitHandler = e =>{
+        e.preventDefault();
+        axios
+        .get(
+          `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${search}&per_page=24&format=json&nojsoncallback=1`
+        )
+        .then(response => {
+          setData(response.data.photos.photo)
+        })
+        .catch(error => {
+          console.log(
+            "Encountered an error with fetching and parsing data",
+            error
+          );
+      })
+      }
+  return (
+    <>
+    <center>
+        <h2>Gallery Snapchat@</h2>
+        <form onSubmit={submitHandler}>
+            <input size='30' type='text' value={search} onChange={changeHandler}/> <br/> <br/>
+            <input type='submit' name='Search'/>
+        </form>
+        <br/>
+        {data.length>=1?<Gallery data={data}/>:<h4>No Data Loaded!!</h4>}
+    </center>
+    </>
+  )
+}
+
+export default Comp
---
+++ src/Gallery.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Gallery = ({ data }) => {
+  return (
+    <div className="row">
+      {data.map((image) => (
+        <div key={image.id}>
+          <div className="col-md-4">
+            <div className="card">
+              <img
+                src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}_m.jpg`}
+                height="200"
+                width="250"
+                alt={image.title}
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Gallery;
